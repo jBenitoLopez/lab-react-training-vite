c0dc7ab21b1d964dfae0d79edfd5cef213c37c47
jBenitoLopez <lopez.jbenito@gmail.com>
2023-07-04 18:58:18 +0200

    fix: tailwind remove

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -3,7 +3,7 @@ import '../styles/IdCard.css'
 
 
 function IdCard({ lastName, firstName, gender, height, birth, picture }) {
-
+    const birthDate = new Date(birth)
   return (
     <div className='card' style={{width: "18rem"}}>
       <img src={picture} className="card-img-top" alt={firstName} />
@@ -11,11 +11,10 @@ function IdCard({ lastName, firstName, gender, height, birth, picture }) {
       <div className="card-body">
         <h5 className="card-title">{firstName} {lastName}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">{gender}</h6>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p className="card-text">Some quick example text to build on the card title of the card's content.</p>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">{height}</li>
-          <li className="list-group-item">{birth}</li>
-          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">Height: {height} cm</li>
+          <li className="list-group-item">Birth Year: {birthDate.getFullYear()}</li>
         </ul>
 
       </div>
